aec29995f1045ba96b3a30f6a375f88bcfdafa3f
dhzdhd <ragas5432100@gmail.com>
2024-04-15 18:46:10 +0530

    Integrate provider in UI

--- lib/src/home/views/home_view.dart
+++ lib/src/home/views/home_view.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:exptrack/src/home/controllers/subscription_controller.dart';
+import 'package:exptrack/src/home/models/transaction_model.dart';
 import 'package:exptrack/src/home/views/pages/expenses.dart';
 import 'package:exptrack/src/home/views/pages/subscriptions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:intl/intl.dart';
 
 import '../../settings/views/settings_view.dart';
@@ -27,6 +30,8 @@ class _HomeViewState extends ConsumerState<HomeView> {
   Page _currentPage = Page.values[0];
   late final TextEditingController _titleController;
   late final TextEditingController _amountController;
+  String currency = getCurrency().$1;
+  DurationSegment selectedSegment = DurationSegment.month;
 
   @override
   void initState() {
@@ -42,13 +47,15 @@ class _HomeViewState extends ConsumerState<HomeView> {
     super.dispose();
   }
 
-  (String, String?) getCurrency() {
+  static (String, String?) getCurrency() {
     final format = NumberFormat.simpleCurrency(locale: Platform.localeName);
     return (format.currencySymbol, format.currencyName);
   }
 
   @override
   Widget build(BuildContext context) {
+    final transactionNotifier = ref.read(subscriptionProvider.notifier);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(_currentPage.name.capitalize()),
@@ -86,95 +93,136 @@ class _HomeViewState extends ConsumerState<HomeView> {
             isScrollControlled: true,
             context: context,
             builder: (context) {
-              return Padding(
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height - 300,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(24.0),
-                          child: ListView(
-                            children: <Widget>[
-                              Text(
-                                'New subscription',
-                                style:
-                                    Theme.of(context).textTheme.headlineMedium,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 16.0, bottom: 8.0),
-                                child: TextField(
-                                  controller: _titleController,
-                                  decoration: const InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    labelText: 'Title',
+              return StatefulBuilder(builder: (context, setModalState) {
+                return Padding(
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height - 300,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(24.0),
+                            child: ListView(
+                              children: <Widget>[
+                                Text(
+                                  'New subscription',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headlineMedium,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 16.0, bottom: 8.0),
+                                  child: TextField(
+                                    controller: _titleController,
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      labelText: 'Title',
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 8.0, bottom: 8.0),
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: TextField(
-                                        controller: _amountController,
-                                        keyboardType: TextInputType.number,
-                                        decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          labelText: 'Amount',
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 8.0, bottom: 8.0),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        child: TextField(
+                                          controller: _amountController,
+                                          keyboardType: TextInputType.number,
+                                          decoration: const InputDecoration(
+                                            border: OutlineInputBorder(),
+                                            labelText: 'Amount',
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    DropdownMenu(
-                                      enableSearch: false,
-                                      initialSelection: getCurrency().$1,
-                                      dropdownMenuEntries: [
-                                        DropdownMenuEntry(
-                                          value: getCurrency().$1,
-                                          label: getCurrency().$1,
+                                      DropdownMenu(
+                                        enableSearch: false,
+                                        initialSelection: currency,
+                                        onSelected: (value) => setModalState(
+                                          () {
+                                            currency = value!;
+                                          },
                                         ),
-                                        const DropdownMenuEntry(
-                                          value: '\$',
-                                          label: '\$',
-                                        ),
-                                      ],
-                                    )
-                                  ],
+                                        dropdownMenuEntries: [
+                                          DropdownMenuEntry(
+                                            value: getCurrency().$1,
+                                            label: getCurrency().$1,
+                                          ),
+                                          const DropdownMenuEntry(
+                                            value: '\$',
+                                            label: '\$',
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              Padding(
-                                padding:
-                                    EdgeInsets.only(top: 16.0, bottom: 8.0),
-                                child: DropdownMenu(
-                                  // width: double.infinity,
-                                  enableSearch: false,
-                                  initialSelection: '',
-                                  label: Text('Hello'),
-                                  dropdownMenuEntries: [],
-                                ),
-                              )
-                            ],
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 16.0,
+                                    bottom: 8.0,
+                                  ),
+                                  child: SegmentedButton<DurationSegment>(
+                                    segments: DurationSegment.values
+                                        .map(
+                                          (e) => ButtonSegment(
+                                            value: e,
+                                            label: Text(e.name.capitalize()),
+                                          ),
+                                        )
+                                        .toList(),
+                                    selected: <DurationSegment>{
+                                      selectedSegment
+                                    },
+                                    showSelectedIcon: false,
+                                    onSelectionChanged: (newSelection) => {
+                                      setModalState(() {
+                                        selectedSegment = newSelection.first;
+                                      })
+                                    },
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: FilledButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Create'),
-                        ),
-                      )
-                    ],
+                        SizedBox(
+                          width: double.infinity,
+                          child: FilledButton(
+                            onPressed: () {
+                              transactionNotifier.addSubscription(
+                                TransactionModel(
+                                  title: _titleController.text,
+                                  desc: 'None',
+                                  amount: int.parse(_amountController.text),
+                                  currency: currency,
+                                  startDate: DateTime.now(),
+                                  duration: Some(switch (selectedSegment) {
+                                    DurationSegment.day =>
+                                      const Duration(days: 1),
+                                    DurationSegment.week =>
+                                      const Duration(days: 7),
+                                    DurationSegment.month =>
+                                      const Duration(days: 30),
+                                    DurationSegment.year =>
+                                      const Duration(days: 365)
+                                  }),
+                                ),
+                              );
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Create'),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              );
+                );
+              });
             },
           );
         },
--- lib/src/home/views/pages/subscriptions.dart
+++ lib/src/home/views/pages/subscriptions.dart
@@ -1,73 +1,117 @@
+import 'package:exptrack/src/home/controllers/subscription_controller.dart';
+import 'package:exptrack/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-enum Segment { all, week, month, year }
+enum DurationSegment { day, week, month, year }
 
-class SubscriptionsPage extends StatefulWidget {
+class SubscriptionsPage extends ConsumerStatefulWidget {
   const SubscriptionsPage({super.key});
 
   @override
-  State<SubscriptionsPage> createState() => _SubscriptionsPageState();
+  ConsumerState<SubscriptionsPage> createState() => _SubscriptionsPageState();
 }
 
-class _SubscriptionsPageState extends State<SubscriptionsPage> {
-  Segment selectedSegment = Segment.all;
+class _SubscriptionsPageState extends ConsumerState<SubscriptionsPage> {
+  DurationSegment selectedSegment = DurationSegment.day;
 
   @override
   Widget build(BuildContext context) {
+    final subscriptions = ref.watch(subscriptionProvider);
+    final totalExpense = subscriptions.isEmpty
+        ? 0
+        : subscriptions
+            .map(
+              (e) => e.amount / e.duration.toNullable()!.inDays,
+            )
+            .reduce(
+              (value, element) => value + element,
+            )
+            .round();
+
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SegmentedButton<Segment>(
-            segments: const [
-              ButtonSegment(
-                value: Segment.all,
-                label: Text('All'),
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Total expense',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        '\$$totalExpense per day',
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ButtonSegment(
-                value: Segment.week,
-                label: Text('Week'),
-              ),
-              ButtonSegment(
-                value: Segment.month,
-                label: Text('Month'),
-              ),
-              ButtonSegment(
-                value: Segment.year,
-                label: Text('Year'),
-              ),
-            ],
-            selected: <Segment>{selectedSegment},
-            showSelectedIcon: false,
-            onSelectionChanged: (newSelection) => {
-              setState(() {
-                selectedSegment = newSelection.first;
-              })
-            },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+            child: SegmentedButton<DurationSegment>(
+              segments: DurationSegment.values
+                  .map(
+                    (e) => ButtonSegment(
+                      value: e,
+                      label: Text(e.name.capitalize()),
+                    ),
+                  )
+                  .toList(),
+              selected: <DurationSegment>{selectedSegment},
+              showSelectedIcon: false,
+              onSelectionChanged: (newSelection) => {
+                setState(() {
+                  selectedSegment = newSelection.first;
+                })
+              },
+            ),
           ),
           Expanded(
             child: ListView(
-              children: [
-                ListTile(
-                  contentPadding: EdgeInsets.zero,
-                  leading: Icon(Icons.home),
-                  title: Text(
-                    'AWS VPS',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleMedium!
-                        .copyWith(fontWeight: FontWeight.normal),
-                  ),
-                  subtitle: Text('26 July 2024'),
-                  trailing: Text(
-                    '\$5 / mo',
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          fontWeight: FontWeight.bold,
+              children: subscriptions.isEmpty
+                  ? [
+                      Text(
+                        'No subscriptions yet',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      )
+                    ]
+                  : subscriptions
+                      .map(
+                        (sub) => ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          title: Text(
+                            sub.title,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(fontWeight: FontWeight.normal),
+                          ),
+                          subtitle:
+                              Text(sub.startDate.toString().split('.')[0]),
+                          trailing: Text(
+                            '${sub.currency}${sub.amount} / ${selectedSegment.name}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
                         ),
-                  ),
-                ),
-              ],
+                      )
+                      .toList(),
             ),
           ),
         ],
